f03d6e35c00a6e09d1b35776e888ce1b30691863
ehuang0709 <ehuang0709@gmail.com>
2024-02-24 12:59:57 -0500

    Implemented fade in effect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 import AboutUs from './AboutUs';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+function useFadeInEffect() {
+  useEffect(() => {
+    const faders = document.querySelectorAll('.fade-in');
+    const appearOptions = {
+      threshold: 0.5,
+    };
+
+    const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
+      entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+          return;
+        } else {
+          entry.target.classList.add('appear');
+          appearOnScroll.unobserve(entry.target);
+        }
+      });
+    }, appearOptions);
+
+    faders.forEach(fader => {
+      appearOnScroll.observe(fader);
+    });
+
+    window.addEventListener('scroll', () => {
+      if (window.scrollY === 0) {
+        faders.forEach(fader => {
+          fader.classList.remove('appear');
+          appearOnScroll.observe(fader);
+        });
+      }
+    });
+
+    // Cleanup function to disconnect the IntersectionObserver when the component unmounts
+    return () => {
+      faders.forEach(fader => appearOnScroll.unobserve(fader));
+      window.removeEventListener('scroll', () => {});
+    };
+  }, []); 
+}
 
 function App() {
 
@@ -17,6 +55,7 @@ function App() {
     }
     setOnClick(!onClick); // Toggle the state to close the menu (if it's open)  
   };   
+
   const [destination, setDestination] = useState(""); 
   const [dates, setDates] = useState(""); 
 
@@ -28,13 +67,15 @@ function App() {
     setDates(event.target.value); 
   }; 
 
+  useFadeInEffect();
+
   return (
     <div className="App">
       <header id='main' className="App-header">
-        <div> Welcome to TravelBuddy! </div>
-        <button class="aboutusbutton" onClick={() => scrollTo('aboutUs')}>About Us</button>
+        <div className="fade-in" > Welcome to TravelBuddy! </div>
+        <button class="aboutusbutton fade-in" onClick={() => scrollTo('aboutUs')}>About Us</button>
       </header>
-      <div id='aboutUs' className="About">
+      <div id='aboutUs' className="About fade-in">
         <AboutUs></AboutUs>
       </div>
 
