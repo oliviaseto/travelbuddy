eb2cc039543fdd4e8d70dabdf6bcae032e8d0056
Olivia Seto <oliviaseto5@gmail.com>
2024-02-24 11:47:28 -0500

    added forms for user input on destination and dates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, { useState } from 'react';
+
 function App() {
+  const [destination, setDestination] = useState(""); 
+  const [dates, setDates] = useState(""); 
+
+  const handleDestinationChange = (event) => {
+    setDestination(event.target.value); 
+  };
+
+  const handleDatesChange = (event) => {
+    setDates(event.target.value); 
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +30,31 @@ function App() {
         >
           Learn React
         </a>
+        <form>
+          <label>
+            Where are you traveling to?
+            <input
+                className='destination_input'
+                type='text'
+                name='destination'
+                placeholder=''
+                value={destination}
+                onChange={handleDestinationChange}
+            />
+          </label>
+          <br />
+          <label>
+            When are you going?
+            <input
+                className='dates_input'
+                type='text'
+                name='dates'
+                placeholder=''
+                value={dates}
+                onChange={handleDatesChange}
+            />
+          </label>
+        </form>
       </header>
     </div>
   );
